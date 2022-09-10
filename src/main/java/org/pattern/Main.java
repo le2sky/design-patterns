dfaba01384500105a89be4537d4d063e238fb1ce
leesky <leehaneul0623@gmail.com>
2022-09-10 23:01:07 +0900

    feat: 옵저버 패턴

--- src/main/java/org/pattern/Main.java
+++ src/main/java/org/pattern/Main.java
@@ -1,10 +1,10 @@
 package org.pattern;
 
-import org.pattern.behavioral.state.StateTest;
+import org.pattern.behavioral.observer.ObserverTest;
 import org.pattern.common.Testable;
 public class Main {
     public static void main(String[] args) {
-        Testable test = new StateTest();
+        Testable test = new ObserverTest();
         test.run();
     }
 }
---
+++ src/main/java/org/pattern/behavioral/observer/RamdomNumberGenerator.java
@@ -0,0 +1,21 @@
+package org.pattern.behavioral.observer;
+
+import java.util.Random;
+
+public class RamdomNumberGenerator extends NumberGenerator{
+    private Random random = new Random();
+    private int number;
+
+    @Override
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public void execute() {
+        for(int i = 0; i < 50; i++) {
+            number = random.nextInt(50);
+            notifyObservers();
+        }
+    }
+}
