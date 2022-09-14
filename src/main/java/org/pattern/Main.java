31c9d09d6007f49f638606578b72c603c32ccdcf
leesky <leehaneul0623@gmail.com>
2022-09-14 23:47:22 +0900

    feat: 비지터 패턴

--- src/main/java/org/pattern/Main.java
+++ src/main/java/org/pattern/Main.java
@@ -1,11 +1,11 @@
 package org.pattern;
 
-import org.pattern.behavioral.iterator.IteratorTest;
+import org.pattern.behavioral.visitor.VisitorTest;
 import org.pattern.common.Testable;
 
 public class Main {
     public static void main(String[] args) {
-        Testable test = new IteratorTest();
+        Testable test = new VisitorTest();
         test.run();
     }
 }
---
+++ src/main/java/org/pattern/behavioral/visitor/Acceptor.java
@@ -0,0 +1,5 @@
+package org.pattern.behavioral.visitor;
+
+public interface Acceptor {
+    public abstract void accept(Visitor v);
+}
