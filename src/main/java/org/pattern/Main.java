0e18bcdd0acb5fa77ac355e9d8696f85ecaa251b
Haneul Lee <hee1149kr@naver.com>
2022-09-17 22:35:43 +0900

    feat: 책임연쇄 패턴 (#18)

--- src/main/java/org/pattern/Main.java
+++ src/main/java/org/pattern/Main.java
@@ -1,11 +1,11 @@
 package org.pattern;
 
-import org.pattern.behavioral.visitor.VisitorTest;
+import org.pattern.behavioral.chainOfReponsibility.ChainOfResponsibilityTest;
 import org.pattern.common.Testable;
 
 public class Main {
     public static void main(String[] args) {
-        Testable test = new VisitorTest();
+        Testable test = new ChainOfResponsibilityTest();
         test.run();
     }
 }
---
+++ src/main/java/org/pattern/behavioral/chainOfReponsibility/Trouble.java
@@ -0,0 +1,17 @@
+package org.pattern.behavioral.chainOfReponsibility;
+
+public class Trouble {
+    private int number;
+    public Trouble(int number) {
+        this.number = number;
+    }
+    public int getNumber() {
+        return number;
+    }
+    @Override
+    public String toString() {
+        return "Trouble{" +
+                "number=" + number +
+                '}';
+    }
+}
